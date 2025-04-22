6f34d9db48682d24f7d2de1a91f480dc18319798
Abdelrahman <abdelrahmanayman0011@gmail.com>
2025-04-23 00:38:31 +0200

    update

--- Assignment5.sol
+++ Assignment5.sol
@@ -5,21 +5,18 @@ contract SimpleMathOperations {
     int256 public A;
     int256 public B;
 
-    // Event declarations
     event VariablesStored(int256 a, int256 b);
     event SumCalculated(int256 sum);
     event DifferenceCalculated(int256 difference);
     event ProductCalculated(int256 product);
     event QuotientCalculated(int256 quotient);
 
-    // Constructor to initialize the variables
     constructor(int256 _a, int256 _b) {
         A = _a;
         B = _b;
         emit VariablesStored(A, B);
     }
 
-    // Setter functions
     function setA(int256 _a) public {
         A = _a;
         emit VariablesStored(A, B);
@@ -30,7 +27,6 @@ contract SimpleMathOperations {
         emit VariablesStored(A, B);
     }
 
-    // Getter functions
     function getA() public view returns (int256) {
         return A;
     }
@@ -39,25 +35,21 @@ contract SimpleMathOperations {
         return B;
     }
 
-    // Function to calculate the sum
-    function calculateSum() public {
+    function calculateSum() public returns (int256) {
         int256 sum = A + B;
         emit SumCalculated(sum);
+        return sum;
     }
 
-    // Function to calculate the difference
     function calculateDifference() public {
         int256 difference = A - B;
         emit DifferenceCalculated(difference);
     }
 
-    // Function to calculate the product
     function calculateProduct() public {
         int256 product = A * B;
         emit ProductCalculated(product);
     }
-
-    // Function to calculate the quotient
     function calculateQuotient() public {
         require(B != 0, "Division by zero is not allowed");
         int256 quotient = A / B;
